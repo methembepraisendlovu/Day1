543e02c1bfd36c21b6be26c59d9fcab01375ff00
methembepraisendlovu <gatshenindlovumethembe@gmail.com>
2025-10-03 17:10:31 +0200

    Day1: Arithemitic operations on two numbers

---
+++ project3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main () {
+	
+   int x ;
+   int y;
+       
+        printf("Enter first number\n");
+        scanf("%d" ,&x);
+       
+       printf("Enter second number\n");
+       scanf("%d" , &y);
+       
+   
+   	printf("x + y = %d\n", x+y);	
+   	printf("x - y = %d\n ", x-y);
+   	printf("x * y = %d\n", x*y);
+   	printf("x / y = %d\n", x/y);
+	
+return 0;	
+	
+	}
